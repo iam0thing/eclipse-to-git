bdeb50b5f36fbffcca020a9d4eab4be99ba9963f
iam0thing <abhishek37singh@hotmail.com>
2019-08-12 07:08:57 +0530

    3rd commit

--- src/guru/i2i/git/HelloGit.java
+++ src/guru/i2i/git/HelloGit.java
@@ -1,15 +1,19 @@
 package guru.i2i.git;
 
 public class HelloGit {
-	
+
 	private String firstName;
 	private String lastName;
+
 	
-
-	public static void main(String[] args) {
-
-      HelloGit helloGit =new HelloGit();
-      
-	}
+	 public static void main(String[] args) {
+	 
+	  HelloGit helloGit =new HelloGit();
+	    helloGit.firstName = "Abhishek";
+	    helloGit.lastName ="Singh";
+	    
+	  
+	  }
+	 
 
 }
